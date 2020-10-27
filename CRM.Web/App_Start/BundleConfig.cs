de7579e267635ea9673d010cacfdb3b7ea93eb8d
git-yasins <2389092255@qq.com>
2020-10-27 15:46:39 +0800

    init

--- CRM.Web/App_Start/BundleConfig.cs
+++ CRM.Web/App_Start/BundleConfig.cs
@@ -21,7 +21,7 @@ namespace CRM
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
-
+     
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
